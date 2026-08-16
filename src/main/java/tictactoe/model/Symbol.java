09e2358bd2c9a51e6b03607bb32ba62984febfa8
Adrian-Ioan Niculae <a.niculae-1@student.tudelft.nl>
2026-08-16 12:18:59 +0300

    feat: display field and getDisplay() getter added feat: constructor added

--- src/main/java/tictactoe/model/Symbol.java
+++ src/main/java/tictactoe/model/Symbol.java
@@ -1,9 +1,18 @@
 package tictactoe.model;
-
 import java.util.*;
 
 public enum Symbol{
-    X,
-    O,
-    EMPTY
+    X("X"),
+    O("O"),
+    EMPTY(" ");
+
+    private final String display;
+
+    Symbol(String display){
+        this.display = display;
+    }
+
+    public String getDisplay(){
+        return display;
+    }
 }
