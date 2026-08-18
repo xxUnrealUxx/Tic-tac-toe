08a974ccd04b8674e5cc7520ceac20e7b88b3ff7
Adrian-Ioan Niculae <a.niculae-1@student.tudelft.nl>
2026-08-18 11:35:50 +0300

    feat: created a controller class to handle the Game and TerminalUI classes together. feat: now the controller handles the flow of the game

---
+++ src/main/java/tictactoe/controller/GameTerminalController.java
@@ -0,0 +1,89 @@
+package tictactoe.controller;
+import tictactoe.model.*;
+import tictactoe.ui.TerminalUI;
+
+public class GameTerminalController {
+
+    private final Game game;
+    private final TerminalUI ui;
+
+    public GameTerminalController(Game game, TerminalUI ui) {
+        this.game = game;
+        this.ui = ui;
+    }
+
+    public void play(){
+        while(true) {
+            int choice = ui.menuChoice();
+            switch (choice) {
+                case 1 -> {
+                    round();
+                    ui.clearScreen();
+                    break;
+                }
+
+                case 2 -> {
+                    changeName();
+                    ui.clearScreen();
+                    break;
+                }
+
+                case 3 -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    public void changeName(){
+        while(true) {
+            Player player1 = game.getPlayers()[0];
+            Player player2 = game.getPlayers()[1];
+            int choice = ui.changeNameChoice(player1.getName(), player2.getName());
+            switch (choice) {
+                case 1 -> {
+                    player1.setName(ui.changeName());
+                    ui.clearScreen();
+                    break;
+                }
+
+                case 2 -> {
+                    player2.setName(ui.changeName());
+                    ui.clearScreen();
+                    break;
+                }
+                case 3 -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    /**
+     * The whole mechanism of the game
+     */
+    public void round() {
+        while(!game.iGameOver()){
+            Player winner = null;
+            ui.clearScreen();
+            ui.printBoard(game.getBoard());
+            ui.playerAnnouncement(game.getCurrentPlayer());
+
+            while(!game.makeMove(ui.chooseCoordinate("Choose row: "), ui.chooseCoordinate("Choose column: "))){
+                ui.printInvalidArgs();
+            }
+
+            winner = game.getWinner();
+            if(winner != null) {
+                ui.printBoard(game.getBoard());
+                ui.announceWinner(winner);
+                game.reset();
+                return;
+            }
+        }
+
+        ui.printBoard(game.getBoard());
+        ui.announceDraw();
+        game.reset();
+    }
+}
