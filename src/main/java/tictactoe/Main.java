39aa0a9fd139318f012d9d6b2e53b2f42a61bb74
Adrian-Ioan Niculae <a.niculae-1@student.tudelft.nl>
2026-09-02 09:46:07 +0300

    refactor: removed the player objects declared in the main function.

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -7,9 +7,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args){
-        Player player1 = new Player("Player1", Symbol.X);
-        Player player2 = new Player("Player2", Symbol.O);
-        Game game = new Game(player1, player2);
+        Game game = new Game();
         TerminalUI ui = new TerminalUI(new Scanner(System.in));
         GameTerminalController controller = new GameTerminalController(game, ui);
         controller.play();
