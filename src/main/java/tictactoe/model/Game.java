73e8434e040b11f6ef0105893b5f017a1a54e916
Adrian-Ioan Niculae <a.niculae-1@student.tudelft.nl>
2026-09-02 10:12:14 +0300

    feat: added offset field refactor: changed constructor to creating 2 new players by default feat: makeMove() uses the offset field refactor: getWinner() uses hasWon() function feat: reset() uses the offset field

--- src/main/java/tictactoe/model/Game.java
+++ src/main/java/tictactoe/model/Game.java
@@ -5,22 +5,27 @@ public class Game{
     private final Board board;
     private final Player[] players;
     private int counter = 0;
+    private int offset = 0;
 
-    public Game(Player playerX, Player playerO){
+    public Game(){
         this.board = new Board();
         players = new Player[2];
-        this.players[0] = playerX;
-        this.players[1] = playerO;
+        this.players[0] = new Player("Player1", Symbol.X);
+        this.players[1] = new Player("Player2", Symbol.O);
     }
 
     public boolean makeMove(int row, int column){
-        if(!board.placeMove(row, column, players[counter%2].getSymbol())) return false;
+        if(!board.placeMove(row, column, players[(offset + counter)%2].getSymbol())) return false;
         counter++;
         return true;
     }
 
     public Player getWinner(){
-        return Player.getSymbolPlayer(players, board.checkWinner());
+        Player winner = Player.getSymbolPlayer(players, board.checkWinner());
+        if(winner != null)
+            winner.hasWon();
+
+        return winner;
     }
 
     public Player getCurrentPlayer(){
@@ -35,12 +40,13 @@ public class Game{
         return board;
     }
 
-    public boolean iGameOver(){
+    public boolean isGameOver(){
         return counter > 8;
     }
 
     public void reset(){
         counter = 0;
+        offset = (offset + 1) % 2;
         board.clear();
     }
 }
